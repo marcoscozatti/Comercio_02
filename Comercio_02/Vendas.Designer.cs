bf10dc0bd10c5d1830c8fbea020643076de0e509
Marcos.afcozatti <marcoscozatti@gmail.com>
2025-04-09 21:59:57 -0300

    bkp geral dos programas

---
+++ Comercio_02/Vendas.Designer.cs
@@ -0,0 +1,381 @@
+﻿namespace Comercio_02
+{
+    partial class Vendas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.lblQtde = new System.Windows.Forms.Label();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.lblprecounit = new System.Windows.Forms.Label();
+            this.btnBuscaCliente = new System.Windows.Forms.Button();
+            this.txtQtde = new System.Windows.Forms.TextBox();
+            this.btnBuscProd = new System.Windows.Forms.Button();
+            this.txtPrecoUnico = new System.Windows.Forms.TextBox();
+            this.dgVendas = new System.Windows.Forms.DataGridView();
+            this.txtTotalComDesconto = new System.Windows.Forms.TextBox();
+            this.txtTotalSemDesconto = new System.Windows.Forms.TextBox();
+            this.txtDesconto = new System.Windows.Forms.TextBox();
+            this.txtDataCompra = new System.Windows.Forms.TextBox();
+            this.txtNomeCliente = new System.Windows.Forms.TextBox();
+            this.txtidCliente = new System.Windows.Forms.TextBox();
+            this.txtNomeProduto = new System.Windows.Forms.TextBox();
+            this.txtidProd = new System.Windows.Forms.TextBox();
+            this.txtidVendas = new System.Windows.Forms.TextBox();
+            this.lbltotalcomDesconto = new System.Windows.Forms.Label();
+            this.lbltotalsemdesconto = new System.Windows.Forms.Label();
+            this.lbldesconto = new System.Windows.Forms.Label();
+            this.lbldatacompra = new System.Windows.Forms.Label();
+            this.lblidcliente = new System.Windows.Forms.Label();
+            this.lblIDProd = new System.Windows.Forms.Label();
+            this.lblIDVendas = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgVendas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Black", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, -2);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(377, 106);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "VENDAS";
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(149, 409);
+            this.btnExcluir.Margin = new System.Windows.Forms.Padding(2);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(56, 24);
+            this.btnExcluir.TabIndex = 30;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Location = new System.Drawing.Point(88, 409);
+            this.btnAlterar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(56, 24);
+            this.btnAlterar.TabIndex = 29;
+            this.btnAlterar.Text = "Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            // 
+            // lblQtde
+            // 
+            this.lblQtde.AutoSize = true;
+            this.lblQtde.Location = new System.Drawing.Point(121, 149);
+            this.lblQtde.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblQtde.Name = "lblQtde";
+            this.lblQtde.Size = new System.Drawing.Size(62, 13);
+            this.lblQtde.TabIndex = 7;
+            this.lblQtde.Text = "Quantidade";
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Location = new System.Drawing.Point(25, 409);
+            this.btnAdicionar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(56, 24);
+            this.btnAdicionar.TabIndex = 28;
+            this.btnAdicionar.Text = "Adicionar";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            // 
+            // lblprecounit
+            // 
+            this.lblprecounit.AutoSize = true;
+            this.lblprecounit.Location = new System.Drawing.Point(199, 149);
+            this.lblprecounit.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblprecounit.Name = "lblprecounit";
+            this.lblprecounit.Size = new System.Drawing.Size(74, 13);
+            this.lblprecounit.TabIndex = 9;
+            this.lblprecounit.Text = "Preço Unitário";
+            // 
+            // btnBuscaCliente
+            // 
+            this.btnBuscaCliente.Location = new System.Drawing.Point(148, 100);
+            this.btnBuscaCliente.Margin = new System.Windows.Forms.Padding(2);
+            this.btnBuscaCliente.Name = "btnBuscaCliente";
+            this.btnBuscaCliente.Size = new System.Drawing.Size(21, 19);
+            this.btnBuscaCliente.TabIndex = 27;
+            this.btnBuscaCliente.Text = "...";
+            this.btnBuscaCliente.UseVisualStyleBackColor = true;
+            // 
+            // txtQtde
+            // 
+            this.txtQtde.Location = new System.Drawing.Point(123, 173);
+            this.txtQtde.Margin = new System.Windows.Forms.Padding(2);
+            this.txtQtde.Name = "txtQtde";
+            this.txtQtde.Size = new System.Drawing.Size(60, 20);
+            this.txtQtde.TabIndex = 23;
+            // 
+            // btnBuscProd
+            // 
+            this.btnBuscProd.Location = new System.Drawing.Point(463, 100);
+            this.btnBuscProd.Margin = new System.Windows.Forms.Padding(2);
+            this.btnBuscProd.Name = "btnBuscProd";
+            this.btnBuscProd.Size = new System.Drawing.Size(21, 19);
+            this.btnBuscProd.TabIndex = 26;
+            this.btnBuscProd.Text = "...";
+            this.btnBuscProd.UseVisualStyleBackColor = true;
+            // 
+            // txtPrecoUnico
+            // 
+            this.txtPrecoUnico.Location = new System.Drawing.Point(199, 173);
+            this.txtPrecoUnico.Margin = new System.Windows.Forms.Padding(2);
+            this.txtPrecoUnico.Name = "txtPrecoUnico";
+            this.txtPrecoUnico.Size = new System.Drawing.Size(72, 20);
+            this.txtPrecoUnico.TabIndex = 22;
+            // 
+            // dgVendas
+            // 
+            this.dgVendas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgVendas.Location = new System.Drawing.Point(18, 203);
+            this.dgVendas.Margin = new System.Windows.Forms.Padding(2);
+            this.dgVendas.Name = "dgVendas";
+            this.dgVendas.RowHeadersWidth = 51;
+            this.dgVendas.RowTemplate.Height = 24;
+            this.dgVendas.Size = new System.Drawing.Size(742, 201);
+            this.dgVendas.TabIndex = 25;
+            // 
+            // txtTotalComDesconto
+            // 
+            this.txtTotalComDesconto.Location = new System.Drawing.Point(503, 173);
+            this.txtTotalComDesconto.Margin = new System.Windows.Forms.Padding(2);
+            this.txtTotalComDesconto.Name = "txtTotalComDesconto";
+            this.txtTotalComDesconto.Size = new System.Drawing.Size(106, 20);
+            this.txtTotalComDesconto.TabIndex = 21;
+            // 
+            // txtTotalSemDesconto
+            // 
+            this.txtTotalSemDesconto.Location = new System.Drawing.Point(381, 173);
+            this.txtTotalSemDesconto.Margin = new System.Windows.Forms.Padding(2);
+            this.txtTotalSemDesconto.Name = "txtTotalSemDesconto";
+            this.txtTotalSemDesconto.Size = new System.Drawing.Size(106, 20);
+            this.txtTotalSemDesconto.TabIndex = 20;
+            // 
+            // txtDesconto
+            // 
+            this.txtDesconto.Location = new System.Drawing.Point(290, 173);
+            this.txtDesconto.Margin = new System.Windows.Forms.Padding(2);
+            this.txtDesconto.Name = "txtDesconto";
+            this.txtDesconto.Size = new System.Drawing.Size(72, 20);
+            this.txtDesconto.TabIndex = 19;
+            // 
+            // txtDataCompra
+            // 
+            this.txtDataCompra.Location = new System.Drawing.Point(27, 173);
+            this.txtDataCompra.Margin = new System.Windows.Forms.Padding(2);
+            this.txtDataCompra.Name = "txtDataCompra";
+            this.txtDataCompra.Size = new System.Drawing.Size(83, 20);
+            this.txtDataCompra.TabIndex = 24;
+            // 
+            // txtNomeCliente
+            // 
+            this.txtNomeCliente.Location = new System.Drawing.Point(174, 126);
+            this.txtNomeCliente.Margin = new System.Windows.Forms.Padding(2);
+            this.txtNomeCliente.Name = "txtNomeCliente";
+            this.txtNomeCliente.Size = new System.Drawing.Size(254, 20);
+            this.txtNomeCliente.TabIndex = 18;
+            // 
+            // txtidCliente
+            // 
+            this.txtidCliente.Location = new System.Drawing.Point(104, 126);
+            this.txtidCliente.Margin = new System.Windows.Forms.Padding(2);
+            this.txtidCliente.Name = "txtidCliente";
+            this.txtidCliente.Size = new System.Drawing.Size(66, 20);
+            this.txtidCliente.TabIndex = 17;
+            // 
+            // txtNomeProduto
+            // 
+            this.txtNomeProduto.Location = new System.Drawing.Point(491, 126);
+            this.txtNomeProduto.Margin = new System.Windows.Forms.Padding(2);
+            this.txtNomeProduto.Name = "txtNomeProduto";
+            this.txtNomeProduto.Size = new System.Drawing.Size(258, 20);
+            this.txtNomeProduto.TabIndex = 16;
+            // 
+            // txtidProd
+            // 
+            this.txtidProd.Location = new System.Drawing.Point(430, 126);
+            this.txtidProd.Margin = new System.Windows.Forms.Padding(2);
+            this.txtidProd.Name = "txtidProd";
+            this.txtidProd.Size = new System.Drawing.Size(57, 20);
+            this.txtidProd.TabIndex = 15;
+            // 
+            // txtidVendas
+            // 
+            this.txtidVendas.Location = new System.Drawing.Point(27, 126);
+            this.txtidVendas.Margin = new System.Windows.Forms.Padding(2);
+            this.txtidVendas.Name = "txtidVendas";
+            this.txtidVendas.Size = new System.Drawing.Size(57, 20);
+            this.txtidVendas.TabIndex = 14;
+            // 
+            // lbltotalcomDesconto
+            // 
+            this.lbltotalcomDesconto.AutoSize = true;
+            this.lbltotalcomDesconto.Location = new System.Drawing.Point(500, 152);
+            this.lbltotalcomDesconto.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbltotalcomDesconto.Name = "lbltotalcomDesconto";
+            this.lbltotalcomDesconto.Size = new System.Drawing.Size(103, 13);
+            this.lbltotalcomDesconto.TabIndex = 12;
+            this.lbltotalcomDesconto.Text = "Total com Desconto";
+            // 
+            // lbltotalsemdesconto
+            // 
+            this.lbltotalsemdesconto.AutoSize = true;
+            this.lbltotalsemdesconto.Location = new System.Drawing.Point(379, 149);
+            this.lbltotalsemdesconto.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbltotalsemdesconto.Name = "lbltotalsemdesconto";
+            this.lbltotalsemdesconto.Size = new System.Drawing.Size(102, 13);
+            this.lbltotalsemdesconto.TabIndex = 11;
+            this.lbltotalsemdesconto.Text = "Total sem Desconto";
+            // 
+            // lbldesconto
+            // 
+            this.lbldesconto.AutoSize = true;
+            this.lbldesconto.Location = new System.Drawing.Point(293, 149);
+            this.lbldesconto.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbldesconto.Name = "lbldesconto";
+            this.lbldesconto.Size = new System.Drawing.Size(53, 13);
+            this.lbldesconto.TabIndex = 10;
+            this.lbldesconto.Text = "Desconto";
+            // 
+            // lbldatacompra
+            // 
+            this.lbldatacompra.AutoSize = true;
+            this.lbldatacompra.Location = new System.Drawing.Point(25, 149);
+            this.lbldatacompra.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbldatacompra.Name = "lbldatacompra";
+            this.lbldatacompra.Size = new System.Drawing.Size(66, 13);
+            this.lbldatacompra.TabIndex = 8;
+            this.lbldatacompra.Text = "DataCompra";
+            // 
+            // lblidcliente
+            // 
+            this.lblidcliente.AutoSize = true;
+            this.lblidcliente.Location = new System.Drawing.Point(102, 102);
+            this.lblidcliente.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblidcliente.Name = "lblidcliente";
+            this.lblidcliente.Size = new System.Drawing.Size(50, 13);
+            this.lblidcliente.TabIndex = 6;
+            this.lblidcliente.Text = "IDCliente";
+            // 
+            // lblIDProd
+            // 
+            this.lblIDProd.AutoSize = true;
+            this.lblIDProd.Location = new System.Drawing.Point(428, 102);
+            this.lblIDProd.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblIDProd.Name = "lblIDProd";
+            this.lblIDProd.Size = new System.Drawing.Size(40, 13);
+            this.lblIDProd.TabIndex = 13;
+            this.lblIDProd.Text = "IDProd";
+            // 
+            // lblIDVendas
+            // 
+            this.lblIDVendas.AutoSize = true;
+            this.lblIDVendas.Location = new System.Drawing.Point(25, 100);
+            this.lblIDVendas.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblIDVendas.Name = "lblIDVendas";
+            this.lblIDVendas.Size = new System.Drawing.Size(18, 13);
+            this.lblIDVendas.TabIndex = 5;
+            this.lblIDVendas.Text = "ID";
+            // 
+            // Vendas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnAlterar);
+            this.Controls.Add(this.lblQtde);
+            this.Controls.Add(this.btnAdicionar);
+            this.Controls.Add(this.lblprecounit);
+            this.Controls.Add(this.btnBuscaCliente);
+            this.Controls.Add(this.txtQtde);
+            this.Controls.Add(this.btnBuscProd);
+            this.Controls.Add(this.txtPrecoUnico);
+            this.Controls.Add(this.dgVendas);
+            this.Controls.Add(this.txtTotalComDesconto);
+            this.Controls.Add(this.txtTotalSemDesconto);
+            this.Controls.Add(this.txtDesconto);
+            this.Controls.Add(this.txtDataCompra);
+            this.Controls.Add(this.txtNomeCliente);
+            this.Controls.Add(this.txtidCliente);
+            this.Controls.Add(this.txtNomeProduto);
+            this.Controls.Add(this.txtidProd);
+            this.Controls.Add(this.txtidVendas);
+            this.Controls.Add(this.lbltotalcomDesconto);
+            this.Controls.Add(this.lbltotalsemdesconto);
+            this.Controls.Add(this.lbldesconto);
+            this.Controls.Add(this.lbldatacompra);
+            this.Controls.Add(this.lblidcliente);
+            this.Controls.Add(this.lblIDProd);
+            this.Controls.Add(this.lblIDVendas);
+            this.Controls.Add(this.label1);
+            this.Name = "Vendas";
+            this.Text = "Vendas";
+            ((System.ComponentModel.ISupportInitialize)(this.dgVendas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Label lblQtde;
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.Label lblprecounit;
+        private System.Windows.Forms.Button btnBuscaCliente;
+        private System.Windows.Forms.TextBox txtQtde;
+        private System.Windows.Forms.Button btnBuscProd;
+        private System.Windows.Forms.TextBox txtPrecoUnico;
+        private System.Windows.Forms.DataGridView dgVendas;
+        private System.Windows.Forms.TextBox txtTotalComDesconto;
+        private System.Windows.Forms.TextBox txtTotalSemDesconto;
+        private System.Windows.Forms.TextBox txtDesconto;
+        private System.Windows.Forms.TextBox txtDataCompra;
+        private System.Windows.Forms.TextBox txtNomeCliente;
+        private System.Windows.Forms.TextBox txtidCliente;
+        private System.Windows.Forms.TextBox txtNomeProduto;
+        private System.Windows.Forms.TextBox txtidProd;
+        private System.Windows.Forms.TextBox txtidVendas;
+        private System.Windows.Forms.Label lbltotalcomDesconto;
+        private System.Windows.Forms.Label lbltotalsemdesconto;
+        private System.Windows.Forms.Label lbldesconto;
+        private System.Windows.Forms.Label lbldatacompra;
+        private System.Windows.Forms.Label lblidcliente;
+        private System.Windows.Forms.Label lblIDProd;
+        private System.Windows.Forms.Label lblIDVendas;
+    }
+}
